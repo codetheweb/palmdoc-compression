af1de09bb27ae17961bdbbc1753664090ec82a7a
Max Isom <hi@maxisom.me>
2024-03-30 16:49:47 -0700

    Better benches

--- benches/calibre.rs
+++ benches/calibre.rs
@@ -1,31 +1,39 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use lipsum::lipsum;
 use palmdoc_compression::calibre::{compress, decompress};
+use rand::seq::SliceRandom;
 
-fn random_worst_case(c: &mut Criterion) {
-    let input = (0..4096).map(|_| rand::random::<u8>()).collect::<Vec<_>>();
+const CHUNK_SIZE: usize = 4096;
 
-    let mut group = c.benchmark_group("calibre random");
-    group.throughput(criterion::Throughput::Bytes(input.len() as u64));
+fn war_and_peace(c: &mut Criterion) {
+    let text = std::fs::read_to_string("resources/war_and_peace.txt").unwrap();
+    let text = text.as_bytes().to_vec();
+    let chunks = text.chunks_exact(CHUNK_SIZE).collect::<Vec<_>>();
+
+    let mut group = c.benchmark_group("war_and_peace");
+    group.throughput(criterion::Throughput::Bytes(CHUNK_SIZE as u64));
     group.bench_function("decompress", |b| {
-        let compressed = compress(&input);
+        let chunk = chunks.choose(&mut rand::thread_rng()).unwrap();
+        let compressed = compress(&chunk);
 
         b.iter(|| {
             decompress(black_box(&compressed));
         })
     });
     group.bench_function("compress", |b| {
+        let chunk = chunks.choose(&mut rand::thread_rng()).unwrap();
+
         b.iter(|| {
-            compress(black_box(&input));
+            compress(black_box(&chunk));
         })
     });
 }
 
 fn lorem_ipsum(c: &mut Criterion) {
-    let lorem_ipsum = lipsum(4096);
-    let lorem_ipsum = lorem_ipsum.as_bytes()[..4096].to_vec();
+    let lorem_ipsum = lipsum(CHUNK_SIZE);
+    let lorem_ipsum = lorem_ipsum.as_bytes()[..CHUNK_SIZE].to_vec();
 
-    let mut group = c.benchmark_group("calibre lorem ipsum");
+    let mut group = c.benchmark_group("lorem_ipsum");
     group.throughput(criterion::Throughput::Bytes(lorem_ipsum.len() as u64));
     group.bench_function("decompress", |b| {
         let compressed = compress(&lorem_ipsum);
@@ -41,5 +49,5 @@ fn lorem_ipsum(c: &mut Criterion) {
     });
 }
 
-criterion_group!(benches, random_worst_case, lorem_ipsum);
+criterion_group!(benches, war_and_peace, lorem_ipsum);
 criterion_main!(benches);
