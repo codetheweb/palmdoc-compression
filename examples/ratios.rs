663e3e4a65f1514f2c5cb93cc82163493e7b7e79
Max Isom <hi@maxisom.me>
2024-03-30 16:23:32 -0700

    Add compression ratio comparision script

---
+++ examples/ratios.rs
@@ -0,0 +1,26 @@
+use palmdoc_compression::calibre;
+use palmdoc_compression::palmdoc;
+
+fn main() {
+    let text = std::fs::read_to_string("resources/war_and_peace.txt").unwrap();
+    let text = text.as_bytes().to_vec();
+
+    let calibre_compressed = text
+        .clone()
+        .chunks(4096)
+        .flat_map(|chunk| calibre::compress(&chunk))
+        .collect::<Vec<_>>();
+    println!(
+        "Calibre compression ratio: {:.2}",
+        text.len() as f64 / calibre_compressed.len() as f64
+    );
+
+    let library_compressed = text
+        .chunks(4096)
+        .flat_map(|chunk| palmdoc::compress_palmdoc(&chunk))
+        .collect::<Vec<_>>();
+    println!(
+        "Library compression ratio: {:.2}",
+        text.len() as f64 / library_compressed.len() as f64
+    );
+}
