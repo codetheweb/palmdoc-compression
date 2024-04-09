394184ac4623392508e5b9cb4b25aba7b9c62bc0
Max Isom <hi@maxisom.me>
2024-04-08 21:36:23 -0700

    Add fuzz target

---
+++ fuzz/fuzz_targets/palmdoc.rs
@@ -0,0 +1,11 @@
+#![no_main]
+
+use libfuzzer_sys::fuzz_target;
+use palmdoc_compression::palmdoc::{compress_palmdoc, decompress_palmdoc};
+
+fuzz_target!(|data: &[u8]| {
+    let compressed = compress_palmdoc(data);
+    let decompressed = decompress_palmdoc(&compressed);
+
+    assert_eq!(data, &decompressed[..]);
+});
