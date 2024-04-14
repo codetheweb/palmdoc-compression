8396c799a9e31b5ba2a8c18f2f14d815605529e1
Max Isom <codetheweb@users.noreply.github.com>
2024-04-13 22:38:51 -0700

    fix: decompression of null bytes (#8)

--- fuzz/fuzz_targets/palmdoc.rs
+++ fuzz/fuzz_targets/palmdoc.rs
@@ -4,6 +4,10 @@ use libfuzzer_sys::fuzz_target;
 use palmdoc_compression::{compress, decompress};
 
 fuzz_target!(|data: &[u8]| {
+    if data.len() > 4096 {
+        return;
+    }
+
     let compressed = compress(data);
     let decompressed = decompress(&compressed).unwrap();
 
